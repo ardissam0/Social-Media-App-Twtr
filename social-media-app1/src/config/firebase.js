5dcf08cfd9e4dfa6d6dd59db1d3905e6bbd61726
Sam Ardis <ardis.productions@gmail.com>
2021-05-08 14:47:41 -0400

    Added dependencies, set up files and folders, added firebase, started css, and working on implementing sidebar components

---
+++ social-media-app1/src/config/firebase.js
@@ -0,0 +1,19 @@
+  
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDVLxNKCoTwthlLkjuAV2AYOjOIwmKnd88",
+    authDomain: "social-media-app-7d095.firebaseapp.com",
+    databaseURL: "https://social-media-app-7d095.firebaseio.com",
+    projectId: "social-media-app-7d095",
+    storageBucket: "social-media-app-7d095.appspot.com",
+    messagingSenderId: "673734190777",
+    appId: "1:673734190777:web:4fd4d3d2a71f3c18af5d68",
+    measurementId: "G-J1XC4XEPPL"
+  };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db;
